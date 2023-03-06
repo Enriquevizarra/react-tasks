d68cdc5c6c7ff373ee7c8e5b25c52f3ca766573d
Enriquevizarra <vizarramattay@gmail.com>
2023-03-06 10:53:11 -0500

    CHORE: minors change

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -24,7 +24,7 @@ function TaskForm() {
         className="p-10"
         >
       <h1
-        className="text-2xl to-black mb-3 font-bold"
+        className="text-2xl text-pink-600 mb-3 font-bold"
       >
         Create your Task
       </h1>
@@ -34,7 +34,8 @@ function TaskForm() {
             setTitle(e.target.value);
           }}
           value={title}
-          className="bg-slate-300 p-3 w-full mb-2"
+          className="bg-pink-300 p-3 w-full mb-2 
+          placeholder-pink-500 font-semibold rounded text-pink-700 border-2 border-rose-400 focus:border-rose-600 outline-0"
         />
         <textarea
           placeholder="write the description of the task"
@@ -42,10 +43,12 @@ function TaskForm() {
             setDescription(e.target.value)
           }}
           value={description}
-          className="bg-slate-300 p-3 w-full mb-2"
+          className="bg-pink-300 p-3 w-full mb-2
+          placeholder-pink-500 font-semibold resize-none rounded text-pink-700 border-2 border-rose-400 focus:border-rose-600 outline-0"
         />
+
         <button
-           className="bg-green-700 rounded-md py-1 px-3 mt-4 text-teal-900 font-bold hover:bg-white"
+           className="bg-pink-300 rounded-md py-1 px-3 mt-4 text-pink-500 font-bold hover:bg-pink-600 hover:text-pink-200"
         >Save</button>
       </form>
     </div>
